fb33cdf24071106d839c582de59e5cfdde161dc1
AruruMayer <58838088+AruruMayer@users.noreply.github.com>
2019-12-26 16:38:19 +0900

    Update dosukoi.h

--- dosukoi.h
+++ dosukoi.h
@@ -1,1 +1,22 @@
-#pragma once
+//
+
+
+
+
+//
+
+#ifndef __DOSUKOI_H
+#define __DOSUKOI_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+#include <string.h>
+#include <windows.h>
+}
+
+#endif
