8339f1423033d66e63633fd5fe64e96ac900647d
waisa4613 <59243911+waisa4613@users.noreply.github.com>
2020-01-15 23:14:52 +0900

    Update dosukoi.h

--- dosukoi.h
+++ dosukoi.h
@@ -20,3 +20,19 @@ extern "C" {
 }
 
 #endif
+//////////////音についての説明//////////////////
+/*
+mciSendString(" open X alias Y", NULL, 0, NULL);
+「open X」のXに再生したい音楽ファイルを選択。
+「alias Y」のYに変数を入れると次からその変数を使えば指定している音楽ファイルを使ってくれる※「alias Y」を使わないとこの後に使う関数でいちいちファイルの名前を使わないといけない
+
+mciSendString("play Y repeat", NULL, 0, NULL);
+「play Y」とすると一回だけ「play Y repeat」とするとループ再生 
+
+mciSendString("stop Y", NULL, 0, NULL);
+音楽を止める
+
+mciSendString("close Y", NULL, 0, NULL);
+音楽ファイルを閉じる
+*/
+///////////////////////////////////////////////わからなかったらワイサに聞いて
