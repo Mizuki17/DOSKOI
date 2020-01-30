91388def77fcc2bd5ccf64aa77d22efb2838e164
waisa4613 <59243911+waisa4613@users.noreply.github.com>
2020-01-30 15:23:19 +0900

    Update Source.cpp

--- MCIAPIでの音の再生/Source.cpp
+++ MCIAPIでの音の再生/Source.cpp
@@ -5,6 +5,7 @@
 int main(void) 
 {
 	mciSendString(" open 11677.mp3 alias music", NULL, 0, NULL);//11677.mp3という音楽ファイルをオープンしてそれを「music」という変数に入れる
+	mciSendString("setaudio Y volume to 10", null, 0, IntPtr.Zero);
 	mciSendString("play music repeat", NULL, 0, NULL);//「play music」とすると一回だけ「play music repeat」とするとループ再生  ※ここで使っているmusicは変数musicのことである
 	
 	Sleep(5000000);//5000秒待つ  ※この間曲が流れる
